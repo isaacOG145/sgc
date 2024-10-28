33b04dc91a7e1b4fa4dd8ce083c856d9c2d52fe1
isaacOG145 <20233tn145@utez.edu.mx>
2024-10-27 21:47:10 -0600

    models y repositories de role y user

--- sgc/src/main/java/utez/edu/_b/sgc/users/model/User.java
+++ sgc/src/main/java/utez/edu/_b/sgc/users/model/User.java
@@ -1,4 +1,116 @@
 package utez.edu._b.sgc.users.model;
+import jakarta.persistence.*;
+import utez.edu._b.sgc.role.model.Role;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "name", columnDefinition = "VARCHAR(50)")
+    private String name;
+    @Column(name = "last_name", columnDefinition = "VARCHAR(100)")
+    private String lastName;
+    @Column(name = "email", columnDefinition = "VARCHAR(100)")
+    private String email;
+    @Column(name ="phone_number", columnDefinition = "VARCHAR(10)")
+    private String phoneNumber;
+    @Column(name = "password", columnDefinition = "VARCHAR(256)")
+    private String password;
+    @Column(name = "status", columnDefinition = "BOOL DEFAULT TRUE")
+    private boolean status;
+    @Column(name = "create_at", insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public User(){}
+
+    public User(String name, String lastName, String email,String phoneNumber, String password, boolean status) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
---
+++ sgc/src/main/java/utez/edu/_b/sgc/users/model/UserRepository.java
@@ -0,0 +1,10 @@
+package utez.edu._b.sgc.users.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

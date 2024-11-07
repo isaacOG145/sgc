0abdbc727331e8124654bf73f85c4d1dac269200
isaacOG145 <20233tn145@utez.edu.mx>
2024-11-07 06:12:18 -0600

    ajustes en usuario

--- sgc/src/main/java/utez/edu/_b/sgc/security/dto/AuthRequest.java
+++ sgc/src/main/java/utez/edu/_b/sgc/security/dto/AuthRequest.java
@@ -1,18 +1,18 @@
 package utez.edu._b.sgc.security.dto;
 
 public class AuthRequest {
-    private String username;
+    private String email;
     private String password;
 
     public AuthRequest() {
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {

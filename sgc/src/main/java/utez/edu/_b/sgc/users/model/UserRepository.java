50b2fda01d73156140e66221371171b14f6e852f
isaacOG145 <20233tn145@utez.edu.mx>
2024-11-07 23:17:33 -0600

    pruebas fallidas

--- sgc/src/main/java/utez/edu/_b/sgc/users/model/UserRepository.java
+++ sgc/src/main/java/utez/edu/_b/sgc/users/model/UserRepository.java
@@ -10,8 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUserName(String username);
-
     Optional<User> findByEmail(String email);
 
     List<User> findByStatus(boolean status);
